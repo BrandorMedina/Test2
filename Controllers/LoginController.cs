f793dfa9d6aa3338deaca08e54d35607da8d2cd4
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2020-02-04 14:49:13 -0400

    all

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Test1.Areas.Identity.Pages.Account;
+
+namespace Test1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult RedirectResult()
+        {
+            return Redirect("/Identity/Account/Login");
+        }
+    }
+}
